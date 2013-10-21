ae263d1a5b4d0225226380451f35bb5511f9aef2
stranger1 <strm.previr@gmail.com>
2013-10-21 19:58:39 +0300

    Vector ariphmetics instead of loops in 1-1 (old implementation - "slowMF1_1.m")

---
+++ slowMF1_1.m
@@ -0,0 +1,41 @@
+function slowMF1_1( f_x, l )
+%solves the equation u''_x = -f_x; u(0)=u(l)=0 using Fourier's method
+%
+%usage:
+% slowMF1_1( f_x, l )
+
+    h = l/150;
+    x = 0:h:l;
+
+    N = length(x) - 1;
+    
+    lambda = zeros(1, N-1); %eigenvalues
+    for k = 2:N-1
+        lambda(k) = ( 4/(h^2)) * (sin(pi*k*h/(2*l)))^2;
+    end;
+
+    mu_x = zeros(N-1, N-1); %eigenfunctions
+    for k = 2:N-1
+        for i = 2:N-1
+            mu_x(k, i) = sqrt(2/l) * sin(pi*k*x(i)/l);
+        end;
+    end;
+
+    Frr = zeros(1, N-1); %Fourier's decomposition coefficients
+    for k = 2:N-1;
+        for j = 2:N-1
+            Frr(k) += f_x(x(j)) * mu_x(k,j) * h;
+        end;
+    end;
+
+    C = Frr./lambda;
+
+    y = zeros(1, N+1); %solution
+    for i = 2:N-1
+        for k = 2:N-1
+            y(i) += C(k) * mu_x(k, i);
+        end;
+    end;
+
+    plot(x, y);
+end
--- solveMF1_1.m
+++ solveMF1_1.m
@@ -4,38 +4,20 @@ function solveMF1_1( f_x, l )
 %usage:
 %   sovleMF1_1( f_x, l )
 
-    h = l/150;
+    h = l/1500;
     x = 0:h:l;
 
-    N = length(x) - 1;    
+    N = length(x)-1;    
     
-    lambda = zeros(1, N-1); %eigenvalues
-    for k = 2:N-1
-        lambda(k) = ( 4/(h^2)) * (sin(pi*k*h/(2*l)))^2;
-    end;
+    lambda = (4/(h^2)) * (sin(pi*(1:N-1)*h/(2*l))).^2;
 
-    mu_x = zeros(N-1, N-1); %eigenfunctions
-    for k = 2:N-1
-        for i = 2:N-1
-            mu_x(k, i) = sqrt(2/l) * sin(pi*k*x(i)/l);
-        end;
-    end;
+    mu_x = sqrt(2/l) * sin(pi*(1:N-1)' * x(2:N)/l);    
 
-    Frr = zeros(1, N-1); %Fourier's decomposition coefficients
-    for k = 2:N-1;
-        for j = 2:N-1
-            Frr(k) += f_x(x(j)) * mu_x(k,j) * h;
-        end;
-    end;
+    Frr = arrayfun(f_x, x(2:N)) * mu_x * h;
 
     C = Frr./lambda;
+    
+    y = C * mu_x;
 
-    y = zeros(1, N+1); %solution
-    for i = 2:N-1
-        for k = 2:N-1
-            y(i) += C(k) * mu_x(k, i);
-        end; 
-    end;
-
-    plot(x, y);
+    plot(x(1:N-1), y);
 end
